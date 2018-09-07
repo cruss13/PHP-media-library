6e20ff5df5a1865642c4a890954ee948098145e4
Corey Russ <corey.j.russ@gmail.com>
2018-09-07 16:18:05 -0700

    Updates and modifications

--- index.php
+++ index.php
@@ -1,21 +1,35 @@
 <?php 
 
+include("inc/data.php"); 
+include("inc/functions.php"); 
+
 $pageTitle = "Full Media Library";
 $section = null;
 
-include("inc/header.php"); ?>
-		<div class="section catalog random">
+include("inc/header.php");
 
-			<div class="wrapper">
+?>
+		
+		
+<div class="section catalog random">
 
-				<h2>May we suggest something?</h2>
+	<div class="wrapper">
 
-								<ul class="items">
-					<li><a href="details.php?id=201"><img src="img/media/forest_gump.jpg" alt="Forrest Gump"><p>View Details</p></a></li><li><a href="details.php?id=204"><img src="img/media/princess_bride.jpg" alt="The Princess Bride"><p>View Details</p></a></li><li><a href="details.php?id=302"><img src="img/media/elvis_presley.jpg" alt="Elvis Forever"><p>View Details</p></a></li><li><a href="details.php?id=303"><img src="img/media/garth_brooks.jpg" alt="No Fences"><p>View Details</p></a></li>								
-				</ul>
+		<h2>May we suggest something?</h2>
 
-			</div>
+		<ul class="items">
+			
+			<?php 
+			$random = array_rand($catalog, 4);
+            foreach ($random as $id) {
+            	echo get_item_html($id, $catalog[$id]);
+            }
+            ?> 
+		
+		</ul>
 
-		</div>
+	</div>
+
+</div>
 
 <?php include("inc/footer.php"); ?>
